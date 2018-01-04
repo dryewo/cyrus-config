70baaddc13e6717649fd3645638ba8eaf40c93d7
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2018-01-04 23:34:10 +0200

    Add travis config. Add release script.

--- project.clj
+++ project.clj
@@ -8,4 +8,14 @@
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [squeeze "0.3.1"]]}})
+                                  [squeeze "0.3.1"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[cyrus\\\\/config \"[0-9.]*\"\\\\]/[cyrus\\\\/config \"${:version}\"]/" "README.md"]}
+  :release-tasks [["shell" "git" "diff" "--exit-code"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["update-readme-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["vcs" "push"]])
