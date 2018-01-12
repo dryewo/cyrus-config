4db8e47537c62226812e5ec229b61209cd091398
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2018-01-12 19:56:36 +0100

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cyrus/config "0.2.0"
+(defproject cyrus/config "0.2.1"
   :description "Config loading library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
