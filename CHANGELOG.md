1fcb4274f138ba2b13c637868ef788d77ba06782
Dmitrii Balakhonskii <dnomaid@gmail.com>
2018-09-03 02:07:42 +0200

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+### Added
+- _CHANGELOG.md_ created.
+
+## 0.2.1 — 2018-01-12
+Released without _CHANGELOG.md_
+
+
+[Unreleased]: https://github.com/dryewo/cyrus-config/compare/0.2.1...HEAD
--- project.clj
+++ project.clj
@@ -1,21 +1,24 @@
 (defproject cyrus/config "0.2.1"
   :description "Config loading library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/dryewo/cyrus-config"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
-  :plugins [[lein-cloverage "1.0.9"]
+  :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
-            [lein-ancient "0.6.15"]]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [squeeze "0.3.1"]]}}
+                                  [squeeze "0.3.2"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[cyrus\\\\/config \"[0-9.]*\"\\\\]/[cyrus\\\\/config \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
                   ["update-readme-version"]
                   ["vcs" "commit"]
                   ["vcs" "tag"]
                   ["deploy"]
-                  ["vcs" "push"]])
+                  ["vcs" "push"]]
+  :vcs :git)
