fe07935e871f2cb60ef4e86568a67656045d05ca
Dmitrii Balakhonskii <dnomaid@gmail.com>
2018-11-11 14:55:46 +0100

    bump lein plugin versions

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
-            [lein-changelog "0.3.1"]]
+            [lein-changelog "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [squeeze "0.3.2"]
                                   [cheshire "5.8.1"]]}}
